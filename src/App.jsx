47c328ede8b6ac79e7ba81a2725fab4142365050
mitanshu <mitanshug2004@gmail.com>
2025-07-31 21:25:30 +0530

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 // App.jsx - Updated with new multiplayer route
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home";
 import Chessboard from "./pages/Chessboard";
 import MultiplayerRoom from "./pages/MultiplayerRoom";
-import NewMultiplayerGame from "./pages/NewMultiplayerGame";
-import RouteHandler from "./components/RouteHandler";
+import NewMultiplayerGame from "./pages/NewMultiplayerGame"; // New version
 
 function App() {
   return (
-    <BrowserRouter>
-      <RouteHandler />
+    <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/bot" element={<Chessboard />} />
         <Route path="/multiplayer/:roomId" element={<MultiplayerRoom />} />
         <Route path="/play/:roomId" element={<NewMultiplayerGame />} />
-        <Route path="*" element={<Homepage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
