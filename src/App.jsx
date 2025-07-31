cc394cc876d080d4f4f7207d0b933269629cc774
mitanshu <mitanshug2004@gmail.com>
2025-07-31 21:16:41 +0530

    fix vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.jsx - Updated with new multiplayer route
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home";
 import Chessboard from "./pages/Chessboard";
 import MultiplayerRoom from "./pages/MultiplayerRoom";
@@ -8,14 +8,14 @@ import NewMultiplayerGame from "./pages/NewMultiplayerGame"; // New version
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/bot" element={<Chessboard />} />
         <Route path="/multiplayer/:roomId" element={<MultiplayerRoom />} />
         <Route path="/play/:roomId" element={<NewMultiplayerGame />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
