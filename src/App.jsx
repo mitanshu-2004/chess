f0f92c37ae6299bdd4de11ff0345cebc5330876b
mitanshu <mitanshug2004@gmail.com>
2025-09-15 17:30:34 +0530

    health api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import { useEffect } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import axios from "axios"
+
 import Homepage from "./pages/Home"
 import Chessboard from "./pages/Chessboard"
 import MultiplayerRoom from "./pages/MultiplayerRoom"
 import NewMultiplayerGame from "./pages/NewMultiplayerGame"
-import MultiplayerLobby from "./pages/MultiplayerLobby" // Import the new component
+import MultiplayerLobby from "./pages/MultiplayerLobby"
 
 function App() {
+  useEffect(() => {
+    const wakeUpBackend = async () => {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API}/api/health`)
+        console.log("🌐 Backend health:", res.data)
+      } catch (err) {
+        console.error("❌ Health check failed:", err.message)
+      }
+    }
+
+    wakeUpBackend()
+  }, [])
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/bot" element={<Chessboard />} />
-        <Route path="/multiplayer-lobby" element={<MultiplayerLobby />} /> {/* New route */}
+        <Route path="/multiplayer-lobby" element={<MultiplayerLobby />} />
         <Route path="/multiplayer/:roomId" element={<MultiplayerRoom />} />
         <Route path="/play/:roomId" element={<NewMultiplayerGame />} />
       </Routes>
